a2fc7b35a5c2dabaef299848fd101d08d0e8fa9f
scpigo <maximiliyan99@mail.ru>
2025-08-30 21:55:29 +0300

    убрал лишние импорты

--- app/api/views.py
+++ app/api/views.py
@@ -1,5 +1,4 @@
-from rest_framework import generics, status
-from rest_framework.response import Response
+from rest_framework import generics
 from rest_framework.exceptions import NotFound
 from .models import Question, Answer
 from .serializers import QuestionSerializer, AnswerSerializer
